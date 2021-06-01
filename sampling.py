3d506723441313f71d22aa3f65884bc94d8c2138
Amal MACHTALAY <72151716+Amal-MACHTALAY@users.noreply.github.com>
2021-06-01 17:35:35 +0100

    Update sampling.py

--- sampling.py
+++ sampling.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def sample(pdf, nbr_samples, n_max=10**6):
     np.random.seed(45)
     """ genearte a list of random samples from a given pdf
@@ -14,3 +16,12 @@ def sample(pdf, nbr_samples, n_max=10**6):
             samples += [x]
         n+=1
     return sorted(samples)
+
+
+""" test """
+L=1.0
+rho_a=0.05; rho_b=0.95; gama=0.1
+def rho_int(s): # initial density
+    return rho_a+(rho_b-rho_a)*np.exp(-0.5*((s-0.5*L)/gama)**2) # 0<=rho<=rho_jam
+
+pos0=sample(rho_int,20)
