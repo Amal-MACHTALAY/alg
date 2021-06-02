edb35d48d591cf17c60fbfd20c312386b5b15322
Amal MACHTALAY <72151716+Amal-MACHTALAY@users.noreply.github.com>
2021-06-02 14:35:50 +0100

    Update sampling.py

--- sampling.py
+++ sampling.py
@@ -8,29 +8,36 @@ Created on Tue Jun  1 17:49:44 2021
 
 import numpy as np
 
-def sample(pdf, nbr_samples, n_max=10**6):
+""" random samples from 1d distribution """
+
+def sample_1d(pdf, interval, nbr_samples, n_max=10**6):
     np.random.seed(45)
-    """ genearte a list of random samples from a given pdf
-    suggests random samples between 0 and L 
-    and accepts-rejects the suggestion with probability pdf(x) 
+    """ genearte a list of random samples from a given pdf suggests random samples in 'interval' 
+    and accepts-rejects the suggestion with probability pdf(x).
+    We generate two numbers x,y from auniform distribution.  
+    If pdf(x)≤y then we keep x as a sample, otherwise we reject it.
+    In regions where the pdf is high, we are less likely to reject an x, 
+    and so we will get more values in that region. 
     """
     samples=[]
+    low=interval[0]
+    high=interval[1]
     n=0
     while len(samples)<nbr_samples and n<n_max:
-        x=np.random.uniform(low=0,high=L)
+        x=np.random.uniform(low,high)
         new_sample=pdf(x)
         assert new_sample>=0 and new_sample<=1
-        if np.random.uniform(low=0,high=1) <=new_sample:
+        if np.random.uniform(low,high) <=new_sample:
             samples += [x]
         n+=1
     return sorted(samples)
 
 
-""" test """
+""" test 1d """
 L=1.0
 rho_a=0.05; rho_b=0.95; gama=0.1
 def rho_int(s): # initial density
     return rho_a+(rho_b-rho_a)*np.exp(-0.5*((s-0.5*L)/gama)**2) # 0<=rho<=rho_jam
 
-pos0=sample(rho_int,20)
+pos0=sample_1d(rho_int,[0,L],20)
 print(pos0)
