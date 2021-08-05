b8ed96c538641cbb3201acb8c859385961058792
Amal MACHTALAY <72151716+Amal-MACHTALAY@users.noreply.github.com>
2021-08-05 13:32:25 +0100

    Update newton_gmres.py

--- newton_gmres.py
+++ newton_gmres.py
@@ -8,21 +8,15 @@ Created on Sat Jul 24 19:28:40 2021
 
 import numpy as np
 
+def compute_residual(w,x,sigma):
+    return -func(w)-((func(w+sigma*x)-func(w))/sigma)
 
-def norm_two(v):
-    n=len(v)
-    norm=v[0]**2
-    for i in range(1,n):
-        norm+=v[i]**2
-    norm=np.sqrt(norm)
-    return norm
-
-# """ test """  
-# a=np.arange(9) - 4
-# print(a)
-# print(np.linalg.norm(a,ord=2))
-# print(norm_two(a))
-
+def get_Hessenberg_matrix(H,m):
+    h=np.zeros((m+1,m))
+    for s in range(m):
+        for k in range(s+2):
+            h[k,s]=H[s][k]
+    return h
 
 def AxB(X,Y):
     XY=np.zeros((len(X),len(Y[0])))
@@ -35,7 +29,8 @@ def AxB(X,Y):
     return XY
 
 
-def LU(A):
+# def LU(L:'float32[:,:]',U:'float32[:,:]'):
+def LU_decomposition(A):
     """ Compute LU Factorization """
     n = max(len(A),len(A[0]))
     L = np.zeros((n,n),dtype=np.float32)
@@ -52,56 +47,167 @@ def LU(A):
                 U[j,k] = U[j,k] - L[j,i]*U[i,k]
     return L,U
 
-# """ test """  
-# m=np.array([[3,4,-1],[2,0,1],[1,3,-2]])
-# print(m)
-# l,u=LU(m)
-# print("l=",l)
-# print("u=",u)
-# print(AxB(l,u))
 
-def gauss_jordan_inv(A,inv):
-    """ Compute the inverse of A """
-    n = len(A)
 
-    #we need a matrix with double number of columns
-    m = 2*n
-    A1 = np.zeros((n,m))
+def LU_inverse(L,U):
+    n = U.shape[0]
+    u = U.copy()
+    E1 = np.eye(n) # This E1 is used to find the inverse of U
+    for j in range(n-1,-1,-1):
+        E1[j,:] = E1[j,:]/u[j,j]
+        u[j,:] = u[j,:]/u[j,j]
+        for i in range(j-1,-1,-1):
+            E1[i,:] = E1[i,:]-E1[j,:]*u[i,j]
+            u[i,:] = u[i,:]-u[j,:]*u[i,j]
+    
+    m = L.shape[0]
+    l = L.copy()
+    E2 = np.eye(m) # This E2 is used to find the inverse of L
+    for j in range(m):
+        for i in range(j+1,m):
+            E2[i,:] = E2[i,:]-E2[j,:]*l[i,j]
+            l[i,:] = l[i,:]-l[j,:]*l[i,j]
+        
+    return E2,E1
+
+def get_preconditionner(A):
+    # A: Jacobian of fct, Ignoring the forward-backward coupling  parts
+    # the *incomplete LU* decomposition
+    L,U=LU_decomposition(A)
+    inv_L,inv_U=LU_inverse(L,U)
+    M=AxB(inv_U,inv_L)
+
+    return M
+
+def matvec(A,v):
+    n = len(v)
+    Av = np.zeros(n)
     for i in range(n):
+        s = 0.0
         for j in range(n):
-            A1[i,j] = A[i,j]
+              s += A[i,j]*v[j]
+        Av[i] = s
+    return Av
+    
 
-    # making the other half an identity matrix of order n
+def gmres(w0, fct, sigma, tol):
+    a=0
+    while True :
+        a+=1
+        print("*********************************************************\n")
+        print(a)
+        # q=(fct(w0+sigma*w0)-fct(w0))/sigma
+        # print('q=',q)
+        # r=-fct(w0)-q
+        r=-fct(w0)
+        # print('r=',r)
+        v=[]
+        v.append(r/np.linalg.norm(r,ord=2))
+        M=get_preconditionner(Jacobian(w0))
+        H=[]
+        j=0
+        while True :
+            hh=[]
+            print('************ j=',j)
+            xj=matvec(M, v[j])
+            q=(fct(w0+sigma*xj)-fct(w0))/sigma
+            vj=q
+            # print('q=',q)
+            for i in range(j+1):
+                # print('---------------i=',i)
+                hij=np.dot(q,v[i])
+                # print('hij=',hij)
+                hh.append(hij)
+                # print(vj,hij*v[i])
+                vj-=hij*v[i]
+            hjpj=np.linalg.norm(vj,ord=2)
+            hh.append(hjpj)
+            H.append(hh)
+            print('H=',H)
+            m=j
+            # print(hjpj)
+            if hjpj==0.0 :
+                break
+            print('yes1')
+            v.append(vj/hjpj)
+            print('v=',v)
+            print('hh=',hh)
+            print(np.linalg.norm(fct(w0)+q,ord=2))
+            if np.linalg.norm(fct(w0)+q,ord=2)<=tol:
+                break
+            print('yes2')
+            j+=1
+        print('m=',m)
+        h=get_Hessenberg_matrix(H,m)
+        print('h=',h)
+        # calcul of beta*e1
+        beta=np.zeros(m)
+        beta[0]=np.linalg.norm(r,ord=2)
+        print('beta=',beta)
+        # Minimize for y
+        print('v=',v)
+        y=np.linalg.lstsq(h.transpose(),beta,rcond=-1)[0]
+        print('y=',y)
+        # if len(y)>len(v):
+        #     y=y[:-1]
+        w0_new=w0+np.dot(np.asarray(v).transpose(),y)
+        print('w0_new=',w0_new)
+        print('w0=',w0)
+        print('norm=',np.linalg.norm(fct(w0),ord=2))
+        if np.linalg.norm(fct(w0),ord=2)<=tol:
+            break
+        tol=max(0.9*(np.linalg.norm(fct(w0_new),ord=2)/np.linalg.norm(fct(w0),ord=2))**2,0.9*tol**2)
+        print(tol)
+        w0=w0_new
+        
+    # Compute residual
+    # res=np.linalg.norm(beta-np.dot(h,y),ord=2)
+    # print('residual=',res)
+    
+    
+    return w0_new,m
 
-    for i in range(n):        
-         A1[i][i+n] = np.float32(1.0)
 
-    # the gauss jordan elimination
-    for i in range(n):
-        if A1[i][i] == 0.0:
-            print('Divide by zero detected!')
-            return 
-        for j in range(n):
-            if i != j:
-                ratio = A1[j][i]/A1[i][i]
-                for k in range(2*n):
-                    A1[j][k] = A1[j][k] - ratio * A1[i][k]
+from scipy.optimize import fsolve
 
-    # operating on rows to make them as identitie (1 in the diag)
-    for i in range(n):
-        divisor = A1[i][i]
-        for j in range(2*n):
-            A1[i][j] = A1[i][j]/divisor
+def func(x):
 
-    # Our matrix inverse
-    for i in range(n):       
-        for j in range(n):
-            inv[i][j]=A1[i][j+n]
+    return np.array([x[0] * np.cos(x[1]) - 4, x[1] * x[0] - x[1] - 5])
 
-# """ test """  
-# m=np.array([[3,4,-1],[2,0,1],[1,3,-2]])
-# print(m)
-# m_inv=np.zeros((len(m),len(m)))
-# #print(m_inv)
-# gauss_jordan_inv(m,m_inv)
-# print(m_inv)
+root = fsolve(func, [1, 1])
+
+def Jacobian(x):
+    return np.array([[np.cos(x[1]),-x[0] * np.sin(x[1])],[x[1],x[0]-1]])
+
+
+# a=np.array([1, 1, 1])
+# b=np.array([5, 6, 7])
+# print(a)
+# print(a+b)
+# print(func(a))
+# print(np.zeros(3))
+result,k=gmres(np.array([0,0]), func, 0.1, 1e-16)
+print("\n***************** GMRES ******************** \n")
+print('k=',k)
+print(result)
+print(func(result))
+
+print("\n************** Exact *********************\n")
+print(root)
+# array([6.50409711, 0.90841421])
+
+print(func(root))  # func(root) should be almost 0.0.
+# array([ True,  True])
+
+# print("\n**************************************\n")
+# w0=np.array([1,1])
+# F=func(w0)
+# print(F)
+# q=(func(w0+0.1*w0)-func(w0))/0.1
+# print(q)
+# r=-F-q
+# print(r)
+# b=np.linalg.norm(r,ord=2)
+# print(b)
+# v0=r/b
+# print(v0)
